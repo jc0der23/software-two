04a264773fb1af0d3fd2e8620f18f18536c3e1ab
jrobchamp@gmail.com <jrobchamp@gmail.com>
2021-08-13 15:41:39 -0500

    cancel button working

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class Controller {
+    @FXML
+    private Button cancelButton;
+
+    public void cancelButtonOnAction(ActionEvent e) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
 }
